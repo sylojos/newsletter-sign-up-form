840c8c654140d8af5a794fe1b0e7c7c793da9b63
lojossy <263162531+sylojos@users.noreply.github.com>
2026-09-23 22:54:23 +0800

    feat: validate email only on change

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,8 @@ const errorMessage = form.querySelector(".jsErrorMessage");
 
 const emailRegExp = /^[\w.!#$%&'*+/=?^`{|}~-]+@[a-z\d-]+(?:\.[a-z\d-]+)+$/i;
 
+let isChanged = false;
+
 /* 
    add novalidate attribute using JavaScript rather than adding
    directly in HTML, this allow form to have a fallback to default
@@ -21,14 +23,20 @@ function isEmailValid() {
 }
 
 function handleEmailInput() {
-  const emailValidity = isEmailValid();
-  if (!emailValidity) {
-    emailInput.setAttribute("aria-invalid", "true");
-    errorMessage.textContent = "Valid email required";
-  } else {
-    emailInput.removeAttribute("aria-invalid");
-    errorMessage.textContent = "";
+  if (isChanged) {
+    const emailValidity = isEmailValid();
+    if (!emailValidity) {
+      emailInput.setAttribute("aria-invalid", "true");
+      errorMessage.textContent = "Valid email required";
+    } else {
+      emailInput.removeAttribute("aria-invalid");
+      errorMessage.textContent = "";
+    }
   }
 }
+
+emailInput.addEventListener("change", () => {
+  isChanged = true;
+});
 
 emailInput.addEventListener("blur", handleEmailInput);
