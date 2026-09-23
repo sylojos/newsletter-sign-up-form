0e2a2c1b7a744cdd267c71f941b85cedf4279943
lojossy <263162531+sylojos@users.noreply.github.com>
2026-09-23 22:45:25 +0800

    feat: add email validation when loses focus

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+const form = document.querySelector("form");
+const emailInput = form.querySelector(".jsEmailField");
+const errorMessage = form.querySelector(".jsErrorMessage");
+
+const emailRegExp = /^[\w.!#$%&'*+/=?^`{|}~-]+@[a-z\d-]+(?:\.[a-z\d-]+)+$/i;
+
+/* 
+   add novalidate attribute using JavaScript rather than adding
+   directly in HTML, this allow form to have a fallback to default
+   browser validation if JavaScript is disabled or failed to load
+   (does not end up with no validation if JavaScript is failed to load
+   or disabled)
+*/
+form.setAttribute("novalidate", "");
+
+function isEmailValid() {
+  return (
+    emailInput.value.trim().length !== 0 &&
+    emailRegExp.test(emailInput.value.trim())
+  );
+}
+
+function handleEmailInput() {
+  const emailValidity = isEmailValid();
+  if (!emailValidity) {
+    emailInput.setAttribute("aria-invalid", "true");
+    errorMessage.textContent = "Valid email required";
+  } else {
+    emailInput.removeAttribute("aria-invalid");
+    errorMessage.textContent = "";
+  }
+}
+
+emailInput.addEventListener("blur", handleEmailInput);
